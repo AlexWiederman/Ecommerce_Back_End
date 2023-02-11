c5f9b81926e75c318fcb66420695d23891365e13
Alex W <amwiederman15@gmail.com>
2023-02-11 15:20:12 -0500

    still trying to fix tag get function

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('.config/connection.js')
+// const sequelize = require('.config/connection.js')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +15,5 @@ app.use(routes);
 // sequelize.sync({force: true}).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
-}
-)
+});
 // });
