7c976326db5bcccd4961be17ccba7e80d41bc4e9
Alex W <amwiederman15@gmail.com>
2023-02-15 17:08:13 -0500

    everything works

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -73,17 +73,11 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update({
-    product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      tagIds: req.body.tagIds
-  },
-    {
-      where: {
-        id: req.params.id,
-      },
-    })
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
     .then((product) => {
       // find all associated tags from ProductTag
       return ProductTag.findAll({ where: { product_id: req.params.id } });
@@ -111,12 +105,13 @@ router.put('/:id', (req, res) => {
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
-    .then((updatedProductTags) => res.status(200).json(updatedProductTags))
+    .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
       res.status(400).json(err);
     });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
